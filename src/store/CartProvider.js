34558178e70dd0c46c2dcc2cd664b74052d618d6
heshamnaser93 <heshamnaser93@icloud.com>
2022-01-10 16:46:37 +0300

    v0.6

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,7 +1,34 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
-  return <CartContext.Provider>{}</CartContext.Provider>;
+  useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    remnoveItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
